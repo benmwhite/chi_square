af29ce74187f6ebcd0eb779b9ae2e922065e75d3
benmwhite <bw2121@gmail.com>
2014-11-17 11:29:29 -0600

    added critical value and alpha slider

--- server.R
+++ server.R
@@ -4,11 +4,13 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     x <- seq(from = 0, to = 40, by = 0.05)
     y <- dchisq(x, input$deg_f)
+    crit <- qchisq(input$alpha, input$deg_f, lower.tail = FALSE)
     qplot(x, y, geom = "line", xlim = c(0, 40),
-          ylim = c(0, 0.4), 
+          ylim = c(0, 0.3), 
           main = paste("Chi-Square Distribution, df =", 
                        as.character(input$deg_f)),
           xlab = "chi-square", ylab = "density") + 
+      geom_vline(xintercept = crit, color = "red") +
       theme_minimal() 
   })
 })
--- ui.R
+++ ui.R
@@ -8,7 +8,13 @@ shinyUI(fluidPage(
                   min = 1,
                   max = 20,
                   value = 10,
-                  step = 1)
+                  step = 1),
+      sliderInput("alpha",
+                  "significance level:",
+                  min = 0.01,
+                  max = 0.2,
+                  value = 0.05,
+                  step = 0.01)
     ),
     mainPanel(
       plotOutput("distPlot")
